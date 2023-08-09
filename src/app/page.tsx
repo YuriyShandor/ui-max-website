3feadeb773314b06cdce14d878cf9283194ef176
YuriyShandor <yuriyshandor@gmail.com>
2023-08-09 18:24:06 +0300

    prod build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
+import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.homePage}>
       <h1 className={styles.heading}>Home Page</h1>
     </div>
-  )
+  );
 }

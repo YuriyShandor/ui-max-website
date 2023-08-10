b3f8f621dc8bc601077f8a97edba4bb21acc2aa6
YuriyShandor <yuriyshandor@gmail.com>
2023-08-10 18:13:21 +0300

    add header

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -5,24 +5,20 @@ import Moon from '@/../public/moon-icon.svg';
 
 import React, { useContext } from 'react';
 import styles from './themeToggle.module.scss';
-import { LIGHT_THEME, ThemeContext } from '@/context/ThemeContext';
+import { ThemeContext } from '@/context/ThemeContext';
 
 const ThemeToggle = () => {
   const { activeTheme, toggleTheme } = useContext(ThemeContext);
-  console.log();
 
   return (
-    <div className={`${styles.themeToggle} ${activeTheme}`} onClick={toggleTheme}>
-      <div className={styles.icon}>
+    <div className={`${styles.themeToggle} ${styles[activeTheme]}`} onClick={toggleTheme}>
+      <div className={`${styles.icon} ${styles.sun} ${styles[activeTheme]}`}>
         <Sun />
       </div>
-      <div
-        className={styles.toggler}
-        style={activeTheme === LIGHT_THEME ? { left: '2px' } : { right: '2px' }}
-      />
-      <div className={styles.icon}>
+      <div className={`${styles.icon} ${styles.moon} ${styles[activeTheme]}`}>
         <Moon />
       </div>
+      <div className={`${styles.toggler} ${styles[activeTheme]}`} />
     </div>
   );
 };

f149043afada28ceb80c5864ee0d9368509a766e
YuriyShandor <yuriyshandor@gmail.com>
2023-08-14 12:15:01 +0300

    add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,19 +4,29 @@ import Image from 'next/image';
 import styles from './header.module.scss';
 import WebsiteLogo from '@/../public/website-logo.png';
 import ThemeToggle from '@/components/ThemeToggle/ThemeToggle';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setIsScrolling(window.scrollY > 80);
+    });
+  }, []);
+
   return (
-    <div className={styles.header}>
-      <div className={styles.headerContainer}>
+    <div className={`${styles.header} header`}>
+      <div className={`${styles.headerContainer} ${isScrolling ? styles['isScrolling'] : ''}`}>
         <div className={styles.headerLogo}>
           <Image src={WebsiteLogo} alt="Website Logo" fill={true} />
         </div>
-        <ThemeToggle />
-        <div className={styles.headerBurger}>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className={styles.headerButtons}>
+          <ThemeToggle />
+          <div className={styles.headerBurger}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
